73ce8e52a5eb8a1732b018d814990e5ca0441b77
Arthur <arthur@IT024165.(none)>
2015-06-02 11:27:23 +0100

    Tidied up progress reporting

--- avoidopt2.py
+++ avoidopt2.py
@@ -88,16 +88,18 @@ def lpTest():
 
 class BbNode:
 
-    def __init__(self,trajlp,bound,steps):
+    def __init__(self,trajlp,bound,steps,verbosity=2):
         self.trajlp = trajlp
         self.bound = bound
         self.steps = steps
+        self.verbosity = verbosity
         self.id = ''
 
     def solve(self):
         self.result = self.trajlp.solve()
 	self.bound = self.trajlp.objValue
-	print self.id + (" LP result = %i" % self.result)
+        if self.verbosity>=2:
+    	    print self.id + (" LP result = %i" % self.result)
         return self.result
 
     def deepcopy(self):
@@ -145,55 +147,67 @@ class AvoidOpt:
                  pstart=[0.0,0.0,0.0,0.0], pgoal=[1.5,1.0,0.0,0.0],
                  xbounds=(-10.0,10.0),ybounds=(-10.0,10.0),
                  obs = [0.45, 1.0, 0.25, 0.6],
-                 maxsolves=1000):
+                 maxsolves=1000, verbosity=1):
         self.obs = obs
 	# create the completely relaxed LP and make it the only active node
         rootlp = LpTraj(Nt,dt,amax,pstart,pgoal,xbounds,ybounds)
-        self.bblist = [BbNode(rootlp,-np.inf,range(Nt))]
+        self.bblist = [BbNode(rootlp,-np.inf,range(Nt),verbosity)]
         inccost=np.inf
 
         for ii in range(maxsolves):
+            if verbosity>=1:
+                print("Node %i : inc %f : node list %i" % (ii, inccost, len(self.bblist)))
             if len(self.bblist)<1:
                 print "Optimal solution found after %i nodes." % ii
                 break
-            print("Num active nodes = %i" % len(self.bblist))
+            if verbosity>=2:
+                 print("Num active nodes = %i" % len(self.bblist))
             # depth first - so grab last node
             thisNode = self.bblist.pop()
             # check if it still needs solving
             if thisNode.bound>inccost:
                 # fathomed
-                print("LP bound above incumbent")
+                if verbosity>=2:
+                    print("LP bound above incumbent")
                 continue
             # solve the thing
 	    thisNode.solve()
             # if it was infeasible
             if thisNode.result<0:
                 # also fathomed
-                print("Infeasible")
+                if verbosity>=2:
+                    print("Infeasible")
                 continue
             if thisNode.bound>inccost:
                 # fathomed again
-                print("LP result above incumbent")
+                if verbosity>=2:
+                    print("LP result above incumbent")
                 continue
             # now check for unsatisfied avoidance constraints
-	    print "Checking steps"
- 	    print thisNode.steps
+	    if verbosity>=2:
+                print "Checking steps"
+ 	        print thisNode.steps
             for kk in thisNode.steps:
                 # is it in the box?
                 if thisNode.trajlp.x[kk].varValue<obs[0] and thisNode.trajlp.x[kk+1].varValue<obs[0]:
-                    print "Step %i clear left" % kk
+                    if verbosity>=2:
+                        print "Step %i clear left" % kk
                     continue
                 elif thisNode.trajlp.x[kk].varValue>obs[1] and thisNode.trajlp.x[kk+1].varValue>obs[1]:
-                    print "Step %i clear right" % kk
+                    if verbosity>=2:
+                        print "Step %i clear right" % kk
                     continue
                 elif thisNode.trajlp.y[kk].varValue<obs[2] and thisNode.trajlp.y[kk+1].varValue<obs[2]:
-                    print "Step %i clear down" % kk
+                    if verbosity>=2:
+                        print "Step %i clear down" % kk
                     continue
                 elif thisNode.trajlp.y[kk].varValue>obs[3] and thisNode.trajlp.y[kk+1].varValue>obs[3]:
-                    print "Step %i clear up" % kk
+                    if verbosity>=2:
+                        print "Step %i clear up" % kk
                     continue
                 else:
-                    print("Incursion step %i" % kk)
+                    if verbosity>=2:
+                        print("Incursion step %i" % kk)
                     # I'm inside - branch on the first one found
                     # make the new list of steps
                     thisNode.steps.remove(kk)
@@ -201,35 +215,41 @@ class AvoidOpt:
                     p1 = thisNode.deepcopy()
                     p1.trajlp.lp.addConstraint(p1.trajlp.x[kk]<=obs[0])
                     p1.trajlp.lp.addConstraint(p1.trajlp.x[kk+1]<=obs[0])
-                    p1.id = p1.id + "%iL" % kk
+                    if verbosity>=2:
+                        p1.id = p1.id + "%iL" % kk
                     self.bblist.append(p1)
                     # right
                     p2 = thisNode.deepcopy()
                     p2.trajlp.lp.addConstraint(p2.trajlp.x[kk]>=obs[1])
                     p2.trajlp.lp.addConstraint(p2.trajlp.x[kk+1]>=obs[1])
-                    p2.id = p2.id + "%iR" % kk
+                    if verbosity>=2:
+                        p2.id = p2.id + "%iR" % kk
                     self.bblist.append(p2)
                     # down
                     p3 = thisNode.deepcopy()
                     p3.trajlp.lp.addConstraint(p3.trajlp.y[kk]<=obs[2])
                     p3.trajlp.lp.addConstraint(p3.trajlp.y[kk+1]<=obs[2])
-                    p3.id = p3.id + "%iD" % kk
+                    if verbosity>=2:
+                        p3.id = p3.id + "%iD" % kk
                     self.bblist.append(p3)
                     # up
                     p4 = thisNode.deepcopy()
                     p4.trajlp.lp.addConstraint(p4.trajlp.y[kk]>=obs[3])
                     p4.trajlp.lp.addConstraint(p4.trajlp.y[kk+1]>=obs[3])
-                    p4.id = p4.id + "%iU" % kk
+                    if verbosity>=2:
+                        p4.id = p4.id + "%iU" % kk
                     self.bblist.append(p4)
                     # append them to the list
                     # break out of the for loop checking constraints
                     break
             else:
                 #if I got through the loop, this is feasible for avoidance
-                print("Feasible with cost %f" % thisNode.bound)
+                if verbosity>=2:
+                    print("Feasible with cost %f" % thisNode.bound)
                 if thisNode.bound<inccost:
                     # got a new incumbent
-		    print "New incumbent"
+		    if verbosity>=2:
+                        print "New incumbent"
                     inccost=thisNode.bound
                     self.inctrajx=[xv.varValue for xv in thisNode.trajlp.x]
                     self.inctrajy=[yv.varValue for yv in thisNode.trajlp.y]
@@ -247,12 +267,9 @@ class AvoidOpt:
 
 def test():
     testobs = [0.45, 1.80, -0.25, 0.6]
-    res = AvoidOpt(obs=testobs,Nt=20,dt=0.2,pgoal=[2.4,0.5,0.0,-0.5])
+    res = AvoidOpt(obs=testobs,Nt=10,dt=0.4,pgoal=[2.4,0.5,0.0,-0.5])
     #plot
     res.plot()
-    #plt.plot(res.inctrajx,res.inctrajy,'sb-',
-    #         [testobs[0],testobs[0],testobs[1],testobs[1],testobs[0]],[testobs[2],testobs[3],testobs[3],testobs[2],testobs[2]],'r-')
-    #plt.show()
 
 if __name__=="__main__":
     test()
