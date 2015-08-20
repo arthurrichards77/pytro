cd0cec53e2e9aa3408215649a6bed423f6ad4eb5
unknown <aeagr@IT051727.users.bris.ac.uk>
2015-08-20 17:39:27 +0100

    Basic B&B solver now complete and getting same answers as MILP
    Signed-off-by: unknown <aeagr@IT051727.users.bris.ac.uk>

--- ltraj.py
+++ ltraj.py
@@ -29,6 +29,12 @@ class LpProbVectors(pulp.LpProblem):
             self.addConstraint(ee-newvar<=0.0)
         return newvar
 
+    def vectorValue(self,vector_expression):
+        v = []
+        for ee in vector_expression:
+            v.append(ee.value())
+        return np.array(v)
+
 class LTraj(LpProbVectors):
 
     def __init__(self,A,B,Nt,name="NoName",sense=1):
@@ -105,38 +111,96 @@ class LpProbUnionCons(LpProbVectors):
         node = self.node_list.pop()
         return(node)
 
+    def _solInUnionRegion(self,r):
+        v = self.vectorValue(r)
+        if np.max(v)<=0.0:
+            flag_reg = True
+        else:
+            flag_reg = False
+        return flag_reg
+
+    def _unionConSatisfied(self,c):
+        flag_sat = False
+        for r in c:
+            if self._solInUnionRegion(r):
+                flag_sat = True
+                break
+        return flag_sat
+
     def _unionFeasible(self):
-        pass
+        flag_feas = True
+        for ci,cc in enumerate(self.union_cons):
+            if self._unionConSatisfied(cc):
+                continue
+            else:
+                flag_feas = False
+                self.first_violated_union = cc
+                self.first_violated_index = ci
+                break
+        return flag_feas
 
-    def _branch(self,branch_node):
-        pass
+    def _childNode(self):
+        # first just call the existing deepcopy inheritend from LpProblem
+        new_node = LpProbUnionCons(name = self.name, sense = self.sense)
+        # and copy the lower bound and the union constraints across
+        new_node.union_cons = self.union_cons[:]
+        new_node.lower_bound = self.lower_bound
+        # copy objective
+        new_node.objective = self.objective.copy()
+        # copy constraints
+        new_node.constraints = self.constraints.copy()
+        # note not bothering to copy node list, as only relevant for root
+        return new_node
 
-    def solveByBranchBound(self,Nmaxnodes=100):
+    def _branch(self,parent_node):
+        parent_node.lower_bound = parent_node.objective.value()
+        for rr in parent_node.first_violated_union:
+            new_node = parent_node._childNode()
+            new_node.union_cons.pop(parent_node.first_violated_index)
+            new_node.addVecLessEqZeroConstraint(rr)
+            self.node_list.append(new_node)
+
+    def solveByBranchBound(self,Nmaxnodes=1000):
         # no lower bound yet
         self.lower_bound = -np.inf
         # initialize the node list with root ULP
-        self.node_list=[self.deepcopy()]
+        self.node_list=[self._childNode()]
         # incumbent
         self.incumbent_cost=np.inf
         # loop
-        for nn in range(Nmaxnodes):
+        for nn in range(Nmaxnodes):            
+            if len(self.node_list)==0:
+                # finished - no more nodes
+                print "%i : %i : %f : OPTIMAL no more nodes" % (nn,len(self.node_list),self.incumbent_cost)
+                # copy result back to parent
+                for ii in range(len(self.variables())):
+                    self.variables()[ii].varValue = self.incumbent_sol[ii]
+                break
             this_node = self._getNextNode()
             if this_node.lower_bound >= self.incumbent_cost:
                 # fathomed as can't improve
+                print "%i : %i : %f : Fathom before solving bound=%f" % (nn,len(self.node_list),self.incumbent_cost,this_node.lower_bound)
                 continue
-            this_node.solve()
+            this_node.solve(solver=pulp.GUROBI())
             if this_node.status < 0:
                 # fathomed as infeasible
+                print "%i : %i : %f : Fathom infeasible status=%i" % (nn,len(self.node_list),self.incumbent_cost,this_node.status)
                 continue
             if this_node.objective.value() >= self.incumbent_cost:
                 # fathomed as did not improve
+                print "%i : %i : %f : Fathom after solving cost=%f" % (nn,len(self.node_list),self.incumbent_cost,this_node.objective.value())
                 continue
             if this_node._unionFeasible():
                 # awesome - this is my new incumbent
                 self.incumbent_cost = this_node.objective.value()
                 self.incumbent_node = this_node
+                self.incumbent_sol = [vv.varValue for vv in this_node.variables()]
+                print "%i : %i : %f : New incumbent %f" % (nn,len(self.node_list),self.incumbent_cost,this_node.objective.value())                
             else:
                 self._branch(this_node)
+                print "%i : %i : %f : Branched with bound=%f" % (nn,len(self.node_list),self.incumbent_cost,this_node.objective.value())                
+        else:
+            print "%i : %i : %f : Node limit reached" % (nn,len(self.node_list),self.incumbent_cost)                
 
     def _convertUnionToMILP(self,uc,M):
         # number of regions
@@ -161,7 +225,7 @@ class LpProbUnionCons(LpProbVectors):
 
     def solveByMILP(self,M=100):
         self._convertToMILP(M)
-        self.solve()
+        self.solve(solver=pulp.GUROBI())
 
 def unionTest():
     lt = LpProbUnionCons()
@@ -212,171 +276,8 @@ def lavTest():
     #lt.addInfNormStageCost(np.zeros((2,2)),np.eye(2))
     lt.add2NormStageCost(np.zeros((2,2)),np.eye(2))
     lt.addStatic2DObst(2.5,3.5,1.5,4.5)
-    lt.addStatic2DObst(5.5,6.5,2.5,7.5)
+    lt.addStatic2DObst(5.5,6.5,3.5,7.5)
     #lt.addInfNormStageCost(np.eye(2),np.zeros((2,2)))
-    lt.solveByMILP()
+    lt.solve()
     lt.plotTraj2D()
     return lt
-
-class AvoidOpt:
-
-    def __init__(self,Nt=5,dt=0.5,amax=2,
-                 pstart=[0.0,0.0,0.0,0.0], pgoal=[1.5,1.0,0.0,0.0],
-                 xbounds=(-10.0,10.0),ybounds=(-10.0,10.0)):
-        self.obststeps = []
-        self.Nt = Nt
-        self.amax = amax
-	# create the completely relaxed LP and make it the only active node
-        self.rootlp = LpTraj(Nt,dt,amax,pstart,pgoal,xbounds,ybounds)
-
-    def addStaticObstacle(self,obsBox):
-        # needs to be added "for all time steps"
-        self.obststeps += [ObstStep(obsBox,kk) for kk in range(self.Nt)]
-        # hack to ensure plotting works during testing
-        self.obs = obsBox
-
-    def _get_next_node(self):
-        # next_node = self.bblist.pop()
-        next_node = self.bblist.pop(0)        
-        return(next_node)
-
-    def solve(self, solver=None, maxsolves=1000, verbosity=1):
-        # initialize branch and bound tree with single root node
-        self.bblist = [BbNode(self.rootlp,-np.inf,self.obststeps,verbosity)]
-        inccost=np.inf
-        # start timer for reporting solution time
-        self.starttime = time.time()
-
-        for ii in range(maxsolves):
-            if verbosity>=1:
-                print("Node %i : inc %f : node list %i" % (ii, inccost, len(self.bblist)))
-            if len(self.bblist)<1:
-                solveTime = time.time()-self.starttime
-                print "Optimal solution found after %i nodes in %f seconds." % (ii, solveTime)
-                break
-            if verbosity>=2:
-                 print("Num active nodes = %i" % len(self.bblist))
-            # depth first - so grab last node
-            thisNode = self._get_next_node()
-            # check if it still needs solving
-            if thisNode.bound>inccost:
-                # fathomed
-                if verbosity>=2:
-                    print("LP bound above incumbent")
-                continue
-            # solve the thing
-            thisNode.solve(solver=solver)
-            # if it was infeasible
-            if thisNode.result<0:
-                # also fathomed
-                if verbosity>=2:
-                    print("Infeasible")
-                continue
-            if thisNode.bound>inccost:
-                # fathomed again
-                if verbosity>=2:
-                    print("LP result above incumbent")
-                continue
-            # now check for unsatisfied avoidance constraints
-	    if verbosity>=2:
-                print "Checking steps" 	        
-            for ob in thisNode.obststeps:
-                obs = ob.obsBox
-                kk = ob.timeStep
-                # is it in the box?
-                if thisNode.trajlp.x[kk].varValue<obs[0] and thisNode.trajlp.x[kk+1].varValue<obs[0]:
-                    if verbosity>=2:
-                        print "Step %i clear left" % kk
-                    continue
-                elif thisNode.trajlp.x[kk].varValue>obs[1] and thisNode.trajlp.x[kk+1].varValue>obs[1]:
-                    if verbosity>=2:
-                        print "Step %i clear right" % kk
-                    continue
-                elif thisNode.trajlp.y[kk].varValue<obs[2] and thisNode.trajlp.y[kk+1].varValue<obs[2]:
-                    if verbosity>=2:
-                        print "Step %i clear down" % kk
-                    continue
-                elif thisNode.trajlp.y[kk].varValue>obs[3] and thisNode.trajlp.y[kk+1].varValue>obs[3]:
-                    if verbosity>=2:
-                        print "Step %i clear up" % kk
-                    continue
-                else:
-                    if verbosity>=2:
-                        print("Incursion step %i" % kk)
-                    # I'm inside - branch on the first one found
-                    # make the new list of steps
-                    thisNode.obststeps.remove(ob)
-                    # four new subproblems - left
-                    p1 = thisNode.deepcopy()
-                    p1.trajlp.lp.addConstraint(p1.trajlp.x[kk]<=obs[0])
-                    p1.trajlp.lp.addConstraint(p1.trajlp.x[kk+1]<=obs[0])
-                    if verbosity>=2:
-                        p1.id = p1.id + "%iL" % kk
-                    self.bblist.append(p1)
-                    # right
-                    p2 = thisNode.deepcopy()
-                    p2.trajlp.lp.addConstraint(p2.trajlp.x[kk]>=obs[1])
-                    p2.trajlp.lp.addConstraint(p2.trajlp.x[kk+1]>=obs[1])
-                    if verbosity>=2:
-                        p2.id = p2.id + "%iR" % kk
-                    self.bblist.append(p2)
-                    # down
-                    p3 = thisNode.deepcopy()
-                    p3.trajlp.lp.addConstraint(p3.trajlp.y[kk]<=obs[2])
-                    p3.trajlp.lp.addConstraint(p3.trajlp.y[kk+1]<=obs[2])
-                    if verbosity>=2:
-                        p3.id = p3.id + "%iD" % kk
-                    self.bblist.append(p3)
-                    # up
-                    p4 = thisNode.deepcopy()
-                    p4.trajlp.lp.addConstraint(p4.trajlp.y[kk]>=obs[3])
-                    p4.trajlp.lp.addConstraint(p4.trajlp.y[kk+1]>=obs[3])
-                    if verbosity>=2:
-                        p4.id = p4.id + "%iU" % kk
-                    self.bblist.append(p4)
-                    # append them to the list
-                    # break out of the for loop checking constraints
-                    break
-            else:
-                #if I got through the loop, this is feasible for avoidance
-                if verbosity>=2:
-                    print("Feasible with cost %f" % thisNode.bound)
-                if thisNode.bound<inccost:
-                    # got a new incumbent
-                    if verbosity>=2:
-                        print "New incumbent"
-                    inccost=thisNode.bound
-                    self.inctrajx=[xv.varValue for xv in thisNode.trajlp.x]
-                    self.inctrajy=[yv.varValue for yv in thisNode.trajlp.y]
-                # plot
-                #plt.plot(self.inctrajx,self.inctrajy,'.b-',
-                #         [obs[0],obs[0],obs[1],obs[1],obs[0]],[obs[2],obs[3],obs[3],obs[2],obs[2]],'r-')
-                #plt.show()
-        else:
-            print "Node limit of %i reached" % ii
-
-    def plot(self):
-        plt.plot(self.inctrajx,self.inctrajy,'sb-')
-        for ob in self.obststeps:
-            obs = ob.obsBox
-            plt.plot([obs[0],obs[0],obs[1],obs[1],obs[0]],[obs[2],obs[3],obs[3],obs[2],obs[2]],'r-')
-        plt.show()
-
-def test():
-    opt = AvoidOpt(Nt=6,dt=1.0,pgoal=[2.4,0.5,0.0,-0.5],amax=100)
-    testobs = [0.45, 1.0, -0.25, 1.9]
-    opt.addStaticObstacle(testobs)
-    testobs = [1.3, 1.9, -0.95, 0.1]
-    opt.addStaticObstacle(testobs)
-    try:
-        # solve by Gurobi, if installed
-        opt.solve(solver=pulp.GUROBI())
-    except PulpSolverError:
-        print("Could not find Gurobi - trying built-in solver")
-        # or solve by PuLP default built-in solver
-        opt.solve()
-    # all being well, plot the trajectory
-    opt.plot()
-
-if __name__=="__main__":
-    test()
