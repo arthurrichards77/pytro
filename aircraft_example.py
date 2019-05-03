879461c7c326b7aaf6aa8a4e826aa05b28dff547
Flying85 <Flying85.User>
2019-05-03 16:50:26 +0100

    Basic minimum time working. TODO: max speed and min speed.

--- aircraft_example.py
+++ aircraft_example.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pulp
 import ltraj
 
 # 2D point mass dynamics
@@ -12,7 +13,7 @@ num_steps = 10
 p = ltraj.LTraj2DAvoid(A,B,num_steps)
 
 # for circle constraints
-Nc=20
+Nc=19
 M = np.transpose(np.vstack((np.cos(2*np.pi*np.array(range(Nc))/Nc),np.sin(2*np.pi*np.array(range(Nc))/Nc))))
 
 # maximum acceleration
@@ -20,22 +21,27 @@ max_acc = 0.4
 C = np.zeros((Nc,4))
 D = M
 e = max_acc*np.ones(Nc)
-print(C)
-print(D)
-print(e)
 p.addStageConstraints(C,D,e)
 
 # set initial state
 p.setInitialState([0.0,0.0,1.0,0.0])
 
 # temp - set fixed terminal state
-term_state = np.array([5.0,5.0,1.0,0.0])
-p.setTerminalState(term_state)
+#term_state = np.array([5.0,5.0,1.0,0.0])
+#p.setTerminalState(term_state)
+
+# minimum time objective
+term_pos = np.array([15.0,10.0])
+# weights
+w_time = 1.0
+w_dist = 10.0
+cost_var = pulp.LpVariable('J')
+p.addUnionConstraint(tuple([w_time*kk+w_dist*np.dot(M,(p.var_x[kk][0:2]-term_pos))-cost_var for kk in range(num_steps)]),name="fin")
+p.objective+=cost_var
 
 # add acceleration cost
-p.add2NormStageCost(np.zeros((2,4)),np.eye(2))
+p.add2NormStageCost(np.zeros((2,4)),0.001*np.eye(2),Nc=17)
 
 print(p)
-p.solveByMILP()
 p.plotTraj2D()
 p.plotStateControlHistory()
