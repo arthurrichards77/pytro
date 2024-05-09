fe0dd4ee737ff5f2fb7c8e28436a0cfccd9b2116
Arthur Richards <aeagr@bristol.ac.uk>
2024-05-09 14:30:46 +0100

    Made example compatible (almost) with Richards and How 2002 Fig. 4

--- aircraft_example.py
+++ aircraft_example.py
@@ -3,35 +3,39 @@ import pulp
 import ltraj
 
 # 2D point mass dynamics
-dt = 1.0
+dt = 2.0
 A = np.vstack(( np.hstack(( np.eye(2),np.eye(2)*dt )), 
                 np.hstack(( np.zeros((2,2)),np.eye(2) )) ))
 B = np.vstack((np.eye(2)*0.5*dt*dt,np.eye(2)*dt))
 
 # basic optimizer
-num_steps = 10
+num_steps = 30
 p = ltraj.LTraj2DAvoid(A,B,num_steps)
 
 # for circle constraints
-Nc=19
+Nc=12
 M = np.transpose(np.vstack((np.cos(2*np.pi*np.array(range(Nc))/Nc),np.sin(2*np.pi*np.array(range(Nc))/Nc))))
 
 # maximum acceleration
-max_acc = 0.4
-C = np.zeros((Nc,4))
-D = M
-e = max_acc*np.ones(Nc)
-p.addStageConstraints(C,D,e)
+max_acc = 0.294/5.0
+# max_acc = 0.5*max_acc # reduced turn capability should result in different route
+Ca = np.zeros((Nc,4))
+Da = M
+ea = max_acc*np.ones(Nc)
+p.addStageConstraints(Ca,Da,ea)
+
+# maximum speed
+max_spd = 0.225
+Cv = np.hstack((np.zeros((Nc,2)),M))
+Dv = np.zeros((Nc,2))
+ev = max_spd*np.ones(Nc)
+p.addStageConstraints(Cv,Dv,ev)
 
 # set initial state
-p.setInitialState([0.0,0.0,1.0,0.0])
-
-# temp - set fixed terminal state
-#term_state = np.array([5.0,5.0,1.0,0.0])
-#p.setTerminalState(term_state)
+p.setInitialState([-5.0,4.0,0.2,0.0])
 
 # minimum time objective
-term_pos = np.array([15.0,10.0])
+term_pos = np.array([5.0,5.0])
 # weights
 w_time = 1.0
 w_dist = 10.0
@@ -42,7 +46,12 @@ p.objective+=cost_var
 # add acceleration cost
 p.add2NormStageCost(np.zeros((2,4)),0.001*np.eye(2),Nc=17)
 
+# add obstacles
+p.addStatic2DObst(-4,-3,3.5,6)
+p.addStatic2DObst(0,1,0,5)
+p.addStatic2DObst(2,3,3,9)
+
 print(p)
-p.solveByMILP(M=1000)
+p.solveByMILP(M=100)
 p.plotTraj2D()
 p.plotStateControlHistory()
